ed279fa34d8cce64c1b6ef30c6ce5710f0293dc8
Dumitrache George <dumitrachegeorge30@gmail.com>
2026-07-17 02:21:48 +0300

    updated winform setup

--- CryptographyToolkit/Form1.cs
+++
@@ -1,9 +0,0 @@
-namespace CryptographyToolkit;
-
-public partial class Form1 : Form
-{
-    public Form1()
-    {
-        InitializeComponent();
-    }
-}
---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace cryptography_toolkit;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
